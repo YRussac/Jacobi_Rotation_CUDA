08d00a4984292dc45866711d2563e69c2d0cd622
MehdiAbbanaBennani <mehdi-b-@live.fr>
2018-05-14 17:31:10 +0200

    Improve main

--- src/constants.h
+++ src/constants.h
@@ -1,7 +1,9 @@
 const int N_PROBLEMS = 1;
 const int N_BLOCKS = 1;
 const int N_THREADS = 256;
-const int GLOBAL_P = 100;
-const int GLOBAL_d = 10;
+const int GLOBAL_P = 3;
+const int GLOBAL_d = 3;
 const int GLOBAL_min_A = 0;
 const int GLOBAL_max_A = 10;
+const int OPTIMISATION = 1;
+const bool DEBUG = true;
