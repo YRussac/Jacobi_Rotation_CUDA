f3b4e5accdc4e8a19b754bc496fc72e9db2c6d85
MehdiAbbanaBennani <mehdi-b-@live.fr>
2018-05-14 16:54:14 +0200

    Add blocks change

--- src/constants.h
+++ src/constants.h
@@ -1,6 +1,7 @@
-const int N_PROBLEMS = 1000;
+const int N_PROBLEMS = 1;
+const int N_BLOCKS = 1;
 const int N_THREADS = 256;
-const int GLOBAL_P = 100000;
-const int GLOBAL_d = 10000;
+const int GLOBAL_P = 100;
+const int GLOBAL_d = 10;
 const int GLOBAL_min_A = 0;
 const int GLOBAL_max_A = 10;
