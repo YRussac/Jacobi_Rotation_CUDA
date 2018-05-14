27554615f34af667981e9f95d2a77a4ef3148207
MehdiAbbanaBennani <mehdi-b-@live.fr>
2018-05-14 14:01:13 +0200

    Complete the code

--- src/constants.h
+++ src/constants.h
@@ -1,5 +1,6 @@
+const int N_PROBLEMS = 1000;
 const int N_THREADS = 256;
-const int GLOBAL_P = 100;
-const int GLOBAL_d = 100;
+const int GLOBAL_P = 100000;
+const int GLOBAL_d = 10000;
 const int GLOBAL_min_A = 0;
 const int GLOBAL_max_A = 10;
